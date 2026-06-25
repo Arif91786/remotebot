3742feb43265854cd038455580ba4cc993529c9d
Arif91786 <nooblonda639@gmail.com>
2026-06-26 00:32:58 +0530

    Fix clone and stdin issue

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg update -y && pkg install python git -y
-git clone https://github.com/Arif91786/remotebot.git
-cd remotebot
+
+# আগের folder থাকলে মুছো
+rm -rf ~/remotebot
+
+git clone https://github.com/Arif91786/remotebot.git ~/remotebot
+cd ~/remotebot
 pip install requests python-telegram-bot==21.5 -q
-python3 install.py
+
+# stdin ঠিক করে চালাও
+python3 install.py < /dev/tty
